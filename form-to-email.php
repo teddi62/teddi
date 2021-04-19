96cbd2e56056431ec3565aef40cd0b477da03588
teddi62 <57998896+teddi62@users.noreply.github.com>
2021-04-19 15:43:21 -0400

    Add files via upload

---
+++ form-to-email.php
@@ -0,0 +1,25 @@
+ <?php
+  $name = $_POST['name'];
+  $visitor_email = $_POST['email'];
+  $message = $_POST['message'];
+?>
+
+<?php
+	$email_from = 'inkedrayss@gmail.com';
+
+	$email_subject = "New Form submission";
+
+	$email_body = "You have received a new message from the user $name.\n".
+                            "Here is the message:\n $message".
+?>
+<?php
+
+  $to = "inkedrayss@gmail.com";
+
+  $headers = "From: $email_from \r\n";
+
+  $headers .= "Reply-To: $visitor_email \r\n";
+
+  mail($to,$email_subject,$email_body,$headers);
+
+ ?>
